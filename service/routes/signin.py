eb4b87187276b0f9f0874eae061d6c224c8ecf03
Nickthephoon <Nicholasphoon.17@ichat.sp.edu.sg>
2019-12-07 16:49:47 +0800

    Updated Signin.py to connect to database

--- service/routes/signin.py
+++ service/routes/signin.py
@@ -1,5 +1,6 @@
 from service import app
 from flask import request
+from service.models import Customer, customers_schema
 import bcrypt
 import jwt
 import json
@@ -18,17 +19,33 @@ def signin():
     key = file.read()
     file.close()
 
-    with open("instance/customers.txt") as json_file:
-        data = json.load(json_file)
-        for p in data["customer"]:
-            if p["username"] == login_username:
+    # with open("instance/customers.txt") as json_file:
+    #     data = json.load(json_file)
+    #     for p in data["customer"]:
+    #         if p["username"] == login_username:
+    #             hashed_password = p["password"]
+    #             if bcrypt.checkpw(
+    #                 login_password.encode("utf-8"),
+    #                 hashed_password.encode("utf-8")
+    #             ):
+    #                 token = jwt.encode(
+    #                     {"username": login_username}, key, algorithm="HS256"
+    #                 )
+    #                 return token
+
+    # return "Login Failed", 401
+
+    customer = Customer.query.all()
+    result = customers_schema.dump(customer)
+    for p in result:
+        if p["name"] == login_username:
                 hashed_password = p["password"]
                 if bcrypt.checkpw(
                     login_password.encode("utf-8"),
                     hashed_password.encode("utf-8")
                 ):
                     token = jwt.encode(
-                        {"username": login_username}, key, algorithm="HS256"
+                    {"name": login_username}, key, algorithm="HS256"
                     )
                     return token
 
