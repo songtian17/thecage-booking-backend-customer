06e41e2d852cbf41aaa4c09ad00552cf67bd34d6
Nickthephoon <Nicholasphoon.17@ichat.sp.edu.sg>
2019-12-07 12:10:28 +0800

    updated config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 DEBUG = False  # Turns on debugging features in Flask
 BCRYPT_LOG_ROUNDS = 12  # Configuration for the Flask-Bcrypt extension
 MAIL_FROM_EMAIL = "robert@example.com"  # For use in application emails
+
+SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Admin123@localhost/FypCopy"
