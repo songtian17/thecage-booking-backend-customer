85fcf7d1a397a5e485eaf43c24c5a79e8325cbb9
cxh <XINGHAN.17@ichat.sp.edu.eg>
2019-12-11 00:08:27 +0800

    Fix import error

--- service/__init__.py
+++ service/__init__.py
@@ -1,10 +1,16 @@
 # app.py or app/__init__.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object("config")  # config.py
 app.config.from_pyfile("config.py")  # instance/config.py
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+ma = Marshmallow(app)
 
 
 @app.route("/")
