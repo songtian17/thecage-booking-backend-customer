35706b54f8527e48e2eb3de16cf10865e7cef9c2
cxh <XINGHAN.17@ichat.sp.edu.eg>
2019-12-16 10:31:48 +0800

    Modify sales order creation to use parameters

--- service/routes/salesordercreate.py
+++ service/routes/salesordercreate.py
@@ -3,29 +3,28 @@ import xmlrpc.client
 import sys
 import json
 import datetime
-from instance.config import url, db, username, password
-from service.models import CustomerOdoo, customer_odoo_schema, customer_odoos_schema
+from instance.config import url, db, username, password, id
+from service.models import Admin, CustomerOdoo, customer_odoo_schema, customer_odoos_schema
 from flask import request, jsonify
 
 
 @app.route("/salesorder/create", methods=["POST"])
 def salesordercreate():
     model_result = ""
-    req_data = request.get_json()
-
-    input_customer_id = req_data["customerId"]
-    input_admin_id = req_data["adminId"]
-
-    customer_odoo = CustomerOdoo.query.get(input_customer_id)
-    customer_id = customer_odoo.odoo_id
 
     common = xmlrpc.client.ServerProxy(f"{url}xmlrpc/2/common")
     uid = common.authenticate(db, username, password, {})
     models = xmlrpc.client.ServerProxy(f"{url}xmlrpc/2/object")
 
-    currentDT = datetime.datetime.now()
-    print(currentDT.strftime("%Y-%m-%d %H:%M:%S"))
+    req_data = request.get_json()
 
+    input_customer_id = req_data["customerId"]
+    customer_odoo = CustomerOdoo.query.filter_by(customer_id=input_customer_id).first()
+    customer_odoo_odoo_id = customer_odoo.odoo_id
+
+    print (id)
+
+    currentDT = datetime.datetime.now()
     model_results = models.execute_kw(
         db,
         uid,
@@ -34,15 +33,8 @@ def salesordercreate():
         "create",
         [
             {
-                # "campaign_id":"false",
-                # "categ_ids":[["6","false",[]]],
                 "categ_ids": [],
-                # "client_order_ref":"false",
                 "date_order": currentDT.strftime("%Y-%m-%d %H:%M:%S"),
-                # "fiscal_position":"false",
-                # "incoterm":"false",
-                # "medium_id":"false",
-                # "message_follower_ids":'false',
                 "message_follower_ids": [
                     [
                         "17541",
@@ -68,25 +60,17 @@ def salesordercreate():
                         "6143",
                     ]
                 ],
-                # "message_ids":"false",
                 "message_ids": [["852511", "852510"]],
                 "note": "This is a computer generated bill and no signature is required. \n\nNo receipt will be issued unless specially requested. \n\nYou may want to print this booking confirmation and bring it along during the day of play. \n\nPlease call us at 63449345 or email us at iwannaplay@thecage.com.sg for any other enquiries. ",
                 "order_line": [],
                 "order_policy": "manual",
-                # "origin":"false",
-                "partner_id": int(customer_id),
-                "partner_invoice_id": int(customer_id),
-                "partner_shipping_id": int(customer_id),
-                # "payment_term":"false",
-                # "paypal_payment_info":"false",
+                "partner_id": int(customer_odoo_odoo_id),
+                "partner_invoice_id": int(customer_odoo_odoo_id),
+                "partner_shipping_id": int(customer_odoo_odoo_id),
                 "picking_policy": "direct",
                 "pricelist_id": "1",
-                # "project_id":"false",
                 "qr_in_report": "true",
-                # "sale_due_date":"false",
-                # "section_id":"false",
-                # "source_id":"false",
-                "user_id": int(input_admin_id),
+                "user_id": int(id),
                 "warehouse_id": "3",
             }
         ],
