26f7e8877f23275df597edb1e3551f7678ab7de0
Nickthephoon <Nicholasphoon.17@ichat.sp.edu.sg>
2019-12-27 16:10:47 +0800

    update customtimeslot.py

--- service/routes/customtimeslot.py
+++ service/routes/customtimeslot.py
@@ -1,7 +1,7 @@
 from flask import jsonify
 from service import app
 from service.models import CustomTimeSlot, customtimeslot_schema, customtimeslots_schema
-
+from service import db
 
 # Get customtimeslot based on Id
 @app.route("/customtimeslot/<Id>", methods=["GET"])
@@ -17,3 +17,13 @@ def get_customtimeslot(field_id):
     all_customtimeslot = CustomTimeSlot.query.filter_by(field_id=field_id).all()
     result = customtimeslots_schema.dump(all_customtimeslot)
     return jsonify(result)
+
+# Delete Custom Timeslot
+@app.route("/customtimeslot/<Id>", methods=["DELETE"])
+def delete_customtimeslot(Id):
+    customtimeslot = CustomTimeSlot.query.get(Id)
+
+    db.session.delete(customtimeslot)
+    db.session.commit()
+
+    return customtimeslot_schema.jsonify(customtimeslot)
