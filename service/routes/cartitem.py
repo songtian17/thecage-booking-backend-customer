b89a5e75bacb21fce5da4db98df3623eb8817af5
cxh <XINGHAN.17@ichat.sp.edu.eg>
2020-01-03 17:54:49 +0800

    Update add_cartitem route

--- service/routes/cartitem.py
+++ service/routes/cartitem.py
@@ -11,86 +11,89 @@ import json
 @app.route("/cartitem", methods=["POST"])
 def add_cartitem():
     token = request.headers["token"]
-    venue_name = request.json["venue"]
-    field_type = request.json["field"]
-    pitch_name = request.json["pitch"]
-    start_time = request.json["timeStart"]
-    end_time = request.json["timeEnd"]
-    expiry_date = datetime.now() + timedelta(minutes=20)
-    product_name = request.json["product"]   
-    code_name = request.json.get("code")
+    items = request.json["items"]
+    for i in items:
+        venue_name = i["venue"]
+        field_type = i["field"]
+        pitch_name = i["pitch"]
+        start_time = i["timeStart"]
+        end_time = i["timeEnd"]
+        expiry_date = datetime.now() + timedelta(minutes=20)
+        product_name = i["product"]   
+        code_name = i.get("code")
 
-    venue = Venue.query.filter_by(name=venue_name).first()
+        venue = Venue.query.filter_by(name=venue_name).first()
 
-    venue_id = venue.id
+        venue_id = venue.id
 
-    product = Product.query.filter_by(name=product_name).first()
-    product_id = product.id
-    amount = product.price
+        product = Product.query.filter_by(name=product_name).first()
+        product_id = product.id
+        amount = product.price
 
-    field = Field.query.filter_by(field_type=field_type, venue_id=venue_id).first()
-    field_id = field.id
+        field = Field.query.filter_by(field_type=field_type, venue_id=venue_id).first()
+        field_id = field.id
 
-    pitch = Pitch.query.filter_by(name=pitch_name, field_id=field_id).first()
-    pitch_id = pitch.id
+        pitch = Pitch.query.filter_by(name=pitch_name, field_id=field_id).first()
+        pitch_id = pitch.id
 
-    promocode = PromoCode.query.filter_by(code=code_name).first()
-    promocode_id = promocode.id
-
-    if code_name is not None:
         promocode = PromoCode.query.filter_by(code=code_name).first()
-        x = promocode.discount_type
-        if x == "Percentage":
-            discount_amount = (100-promocode.discount)*amount/100
-        elif x == "Price":
-            discount_amount = (amount - promocode.discount)
-        else:
-            discount_amount = amount
+        promocode_id = promocode.id
 
-    file = open("instance/key.key", "rb")
-    key = file.read()
-    file.close()
+        if code_name is not None:
+            promocode = PromoCode.query.filter_by(code=code_name).first()
+            x = promocode.discount_type
+            if x == "Percentage":
+                discount_amount = (100-promocode.discount)*amount/100
+            elif x == "Price":
+                discount_amount = (amount - promocode.discount)
+            else:
+                discount_amount = amount
 
-    customer_id = jwt.decode(token, key, algorithms=['HS256'])["customer_id"]
+        file = open("instance/key.key", "rb")
+        key = file.read()
+        file.close()
 
-    newcartitem = CartItem(venue_id, field_id, pitch_id, promocode_id, customer_id, start_time, end_time, expiry_date, product_id, amount, discount_amount)
-    db.session.add(newcartitem)
+        customer_id = jwt.decode(token, key, algorithms=['HS256'])["customer_id"]
+
+        newcartitem = CartItem(venue_id, field_id, pitch_id, promocode_id, customer_id, start_time, end_time, expiry_date, product_id, amount, discount_amount)
+        db.session.add(newcartitem)
+
     db.session.commit()
-    return cart_item_schema.jsonify(newcartitem)
+    return (request.json)
 
 # Update
-@app.route("/cartitem/<Id>", methods=["PUT"])
-def update_cartitem(Id):
-    cartitem = CartItem.query.get(Id) 
-    product_name = request.json["product"]
+# @app.route("/cartitem/<Id>", methods=["PUT"])
+# def update_cartitem(Id):
+#     cartitem = CartItem.query.get(Id) 
+#     product_name = request.json["product"]
 
     
-    product = Product.query.filter_by(name=product_name).first()
-    product_id = product.id
-    amount = product.price
+#     product = Product.query.filter_by(name=product_name).first()
+#     product_id = product.id
+#     amount = product.price
 
-    code_id = cartitem.promocode_id
-    print(code_id)
+#     code_id = cartitem.promocode_id
+#     print(code_id)
 
-    if code_id is not None:
-        promocode = PromoCode.query.get(code_id)
-        x = promocode.discount_type
-        if x == "Percentage":
-            discount_amount = (100-promocode.discount)*amount/100
-        elif x == "Price":
-            discount_amount = (amount - promocode.discount)
-        else:
-            discount_amount = amount
-    else:
-        discount_amount = amount
+#     if code_id is not None:
+#         promocode = PromoCode.query.get(code_id)
+#         x = promocode.discount_type
+#         if x == "Percentage":
+#             discount_amount = (100-promocode.discount)*amount/100
+#         elif x == "Price":
+#             discount_amount = (amount - promocode.discount)
+#         else:
+#             discount_amount = amount
+#     else:
+#         discount_amount = amount
 
-    cartitem.product_id = product_id
-    cartitem.discount_amount = discount_amount
-    cartitem.amount = amount
+#     cartitem.product_id = product_id
+#     cartitem.discount_amount = discount_amount
+#     cartitem.amount = amount
 
-    db.session.commit()
+#     db.session.commit()
 
-    return cart_item_schema.jsonify(cartitem)
+#     return cart_item_schema.jsonify(cartitem)
 
 # Get customer's cart items
 @app.route("/cartitem", methods=["GET"])
