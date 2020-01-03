af6bc4db9fe8e7572a84d9514c45faec31d8db02
cxh <XINGHAN.17@ichat.sp.edu.eg>
2020-01-03 15:23:28 +0800

    Implement odoo api call in /signup route

--- service/routes/signup.py
+++ service/routes/signup.py
@@ -1,13 +1,13 @@
 from service import app
 from flask import request, jsonify
-from service.models import Customer, customer_schema
+from service.models import Customer, customer_schema, customer_schema2, CustomerOdoo, customer_odoo_schema, Venue, venue2s_schema
+from instance.config import url, db as database, username, password
 from service import db
 import bcrypt
 from sqlalchemy import exc
 from parse import parse
 import json
-
-# import json
+import xmlrpc.client
 
 
 @app.route("/signup", methods=["POST"])
@@ -31,4 +31,31 @@ def signup():
         dupe_field = parse('duplicate key value violates unique constraint "{constraint}"\nDETAIL:  Key ({field})=({input}) already exists.\n', str(e.orig))["field"]
         print(dupe_field)
         return json.dumps({'message': f'{dupe_field} already exists'}), 400, {'ContentType': 'application/json'}
-    return customer_schema.jsonify(new_customer)
+
+    common = xmlrpc.client.ServerProxy(f"{url}xmlrpc/2/common")
+    uid = common.authenticate(database, username, password, {})
+    models = xmlrpc.client.ServerProxy(f"{url}xmlrpc/2/object")
+    odoo_customer_id = models.execute_kw(
+        database,
+        uid,
+        password,
+        "res.partner",
+        "create",
+        [
+            {
+                "name": register_username,
+                "email": register_email,
+                "phone": register_phone,
+            }
+        ],
+    )
+
+    venue = Venue.query.order_by(Venue.id).all()
+    results = venue2s_schema.dump(venue)
+
+    for result in results:
+        new_customer_odoo = CustomerOdoo(new_customer.id, odoo_customer_id, result["id"])
+        db.session.add(new_customer_odoo)
+        db.session.commit()
+
+    return customer_schema2.jsonify(new_customer)
