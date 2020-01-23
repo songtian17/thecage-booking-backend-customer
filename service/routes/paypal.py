e3d56231185160a8e1ba6e188d51008c2228427f
cxh <XINGHAN.17@ichat.sp.edu.eg>
2020-01-23 17:32:31 +0800

    Implement paypal route

--- service/routes/paypal.py
+++ service/routes/paypal.py
@@ -1,6 +1,8 @@
 from flask import render_template, jsonify, request
 import paypalrestsdk
 from service import app
+from service.models import Venue, Field, Pitch, Product, CartItem, cart_items_schema
+import jwt
 
 paypalrestsdk.configure({
   "mode": "sandbox",  # sandbox or live
@@ -9,7 +11,31 @@ paypalrestsdk.configure({
 
 @app.route('/payment', methods=['POST'])
 def payment():
+    tokenstr = request.headers["Authorization"]    
+    file = open("instance/key.key", "rb")
+    key = file.read()
+    file.close()
+    tokenstr = tokenstr.split(" ")
+    token = tokenstr[1]
+    customer_id = jwt.decode(token, key, algorithms=['HS256'])["customer_id"]
 
+    items_list = []
+    cartitem = CartItem.query.filter_by(customer_id=customer_id).all()
+    results = cart_items_schema.dump(cartitem)
+    # print(results)
+    total_amount = 0
+    for result in results:
+        item = {}
+        product = Product.query.get(result["product_id"])
+        item["name"] = product.name
+        item["sku"] = product.id
+        item["price"] = result["discounted_amount"]
+        item["description"] = f'{result["venue_id"]}{result["field_id"]}{result["pitch_id"]}{result["start_time"]}{result["end_time"]}'
+        item["currency"] = "SGD"
+        item["quantity"] = 1
+        items_list.append(item)
+        total_amount += result["discounted_amount"]
+    print(items_list)
     payment = paypalrestsdk.Payment({
         "intent": "sale",
         "payer": {
@@ -19,15 +45,17 @@ def payment():
             "cancel_url": "http://localhost:3000/"},
         "transactions": [{
             "item_list": {
-                "items": [{
-                    "name": "testitem",
-                    "sku": "12345",
-                    "price": "500.00",
-                    "currency": "USD",
-                    "quantity": 1}]},
+                "items": items_list},
+                # "items": [{
+                #     "name": "testitem",
+                #     "sku": "12345",
+                #     "price": "10.00",
+                #     "description": "",
+                #     "currency": "SGD",
+                #     "quantity": 1}]},
             "amount": {
-                "total": "500.00",
-                "currency": "USD"},
+                "total": str(total_amount),
+                "currency": "SGD"},
             "description": "This is the payment transaction description."}]})
 
     if payment.create():
