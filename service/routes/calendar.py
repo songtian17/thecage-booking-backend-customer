3edea1a3fec82dcd96e332d2a453b3688395cf39
cxh <XINGHAN.17@ichat.sp.edu.eg>
2020-01-30 15:57:51 +0800

    Add /calendar/week route

--- service/routes/calendar.py
+++ service/routes/calendar.py
@@ -37,9 +37,47 @@ def route1():
         {"fields": ["id", "booking_start", "booking_end", "pitch_id"]},
     )
     for result in model_results:
-        print(result["pitch_id"][0])
+        # print(result["pitch_id"][0])
         db_pitch = Pitch.query.filter_by(odoo_id=result["pitch_id"][0]).first()
-        print(db_pitch.odoo_id)
+        # print(db_pitch.odoo_id)
         result["pitch_id"] = db_pitch.odoo_id
-    print (model_results)
+    # print (model_results)
+    return json.dumps(model_results)
+
+@app.route("/calendar/week", methods=["POST"])
+def route2():
+    model_result = ""
+    req_data = request.get_json()
+
+    booking_date_start = req_data["bookingDateStart"]
+    booking_date_end = req_data["bookingDateEnd"]
+    field_id = req_data["fieldId"]
+
+    result_fields = Field.query.filter_by(id=field_id).first()
+    result = field3_schema.dump(result_fields)
+    common = xmlrpc.client.ServerProxy(f"{url}xmlrpc/2/common")
+    uid = common.authenticate(db, username, password, {})
+    models = xmlrpc.client.ServerProxy(f"{url}xmlrpc/2/object")
+
+    model_results = models.execute_kw(
+        db,
+        uid,
+        password,
+        "sale.order.line",
+        "search_read",
+        [
+            [
+                ["booking_start", ">=", booking_date_start],
+                ["booking_end", "<=", booking_date_end],
+                ["venue_id", "=", result["odoo_id"]],
+            ]
+        ],
+        {"fields": ["id", "booking_start", "booking_end", "pitch_id"]},
+    )
+    for result in model_results:
+        # print(result["pitch_id"][0])
+        db_pitch = Pitch.query.filter_by(odoo_id=result["pitch_id"][0]).first()
+        # print(db_pitch.odoo_id)
+        result["pitch_id"] = db_pitch.odoo_id
+    # print (model_results)
     return json.dumps(model_results)
