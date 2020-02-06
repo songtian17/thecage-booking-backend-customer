d1310b4750008a64a0e41822f5dfed995fa46b51
Nickthephoon <Nicholasphoon.17@ichat.sp.edu.sg>
2020-02-06 17:34:24 +0800

    update models.py

--- service/models.py
+++ service/models.py
@@ -385,8 +385,7 @@ class ProductSchema(ma.Schema):
     odoo_id = fields.Integer(required=True)
     start_time = fields.Time(required=True)
     end_time = fields.Time(required=True)
-    product_valid_day = fields.List(fields.Nested(ProductValidDaySchema(only=("day_of_week"))))
-    
+    product_valid_day = fields.List(fields.Nested(ProductValidDaySchema(only=("id", "day_of_week"))))
 product_schema = ProductSchema()
 products_schema = ProductSchema(many=True)
 
